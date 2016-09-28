cf7561ed06038c8d029b416dc2ca5e389663b30f
YuanStudio <Sijiamomo@126.com>
2016-09-28 09:20:58 +0800

    Initial commit

---
+++ app/src/main/java/com/example/cathleen/testonwayconveydata/AnotherActivity.java
@@ -0,0 +1,37 @@
+package com.example.cathleen.testonwayconveydata;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Toast;
+
+public class AnotherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_another);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        Intent intent = getIntent();
+        String name = intent.getStringExtra("name");
+        int age = intent.getIntExtra("age",18);
+        Toast.makeText(AnotherActivity.this,name + " " + age,Toast.LENGTH_LONG).show();
+
+    }
+
+}
